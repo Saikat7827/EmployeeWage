0454835430633ed34211ab283c26779088070130
Saikat7827 <saikatchakraborty957.sc@gmail.com>
2022-08-27 20:17:16 +0530

    Half Day Wages

--- src/com/bl/Assignment/WageComputation.java
+++ src/com/bl/Assignment/WageComputation.java
@@ -3,27 +3,37 @@ package com.bl.Assignment;
 import java.util.Random;
 
 public class WageComputation {
-		public static void main(String[] args) {
+
+	public static void main(String[] args) {
+		int empHrs = 0;
+		int halfhrs= 4;
+		int wageperhr = 20;
+		Random random = new Random();
+		int randomnum = random.nextInt(2);
+		
+		if (randomnum == 1)
+		{	
+			int salary = halfhrs * wageperhr;
+			empHrs = 4;
+			System.out.println("Employee is Present");
+			//empHrs = 4;
+			int salary1 = halfhrs * wageperhr;
+			System.out.println("Employee Wage is " +salary1);
 			
-			int empHrs = 0;
-			int wageperhr = 20;
-			Random random = new Random();
-			int randomnum = random.nextInt(2);
 			
-			if (randomnum == 1)
-			{
-				System.out.println("Employee is Present");
-				empHrs = 8;
-				
+		}	
+			else {
+				System.out.println("Employee is absent");
+		int salary = empHrs * wageperhr;
+		System.out.println("Employee Wage is" +salary );
 			}
-			else 
-				System.out.println("Employee is Absent");
-			double salary = empHrs * wageperhr;
-			System.out.println("Employee Wage is" +salary );
 		}
-		
-		
+	
+			
 	}
+		
+		
+	
 	
 	
 	
